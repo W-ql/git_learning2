48d172fb174d132cba5bbc56552b25dc1b056785
WQL2 <3176324302@qq.com>
2022-09-04 17:00:42 +0800

    Modify 1.py

--- 1.py
+++ 1.py
@@ -1,16 +1,16 @@
 n,m = 6,8
-data_list = []
+d_list = []
 for i in range(int(n)):
     data = input().split()
     d = [int(data[0]),int(data[1])]
     d = sorted(d)
-    data_list.append(d)
+    d_list.append(d)
 maxvalue = 0
 for i in range(int(n)):
-    data1 = data_list[i]
+    data1 = d_list[i]
     c = 1
     for j in range(i+1,int(n)):
-        data2 = data_list[j]
+        data2 = d_list[j]
         if data1[1] < data2[0]:
             c += 1
     if c > maxvalue:
